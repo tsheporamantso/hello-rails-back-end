9c671d32bfbc45e10c62e0e45c93873691dcbdef
tsheporamantso <tshepo.ramantso@outlook.com>
2024-02-21 16:21:54 +0200

    Correct typing error on on render json, data wasn't recognised as json data or file

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,6 +1,6 @@
 class GreetingsController < ApplicationController
   def random
     random_message = Message.order('RANDOM()').first
-    render json: random_message.text 
+    render json: {greeting: random_message&.text} 
   end
 end
